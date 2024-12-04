484f443dfcd56f539033a7ca9a48c40033e230f5
Peter Hayward <peterhayward1707@gmail.com>
2024-12-04 14:11:18 +0000

    finished input validation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-
-typeArray = [Dog,Cat,Rabbit,Reptile,Bird]
+from datetime import datetime
 
 class Pet:
     def __init__(self, name, birthDate, lastVac):
@@ -10,7 +8,7 @@ class Dog(Pet):
     def getSchedule(self):
         return {
             "name": self.name,
-            "type": "Dog",
+            "animal": "Dog",
             "nextVac": self.getNext(365),
             "nextCheck": self.getNext(365),
         }
@@ -19,7 +17,7 @@ class Cat(Pet):
     def getSchedule(self):
         return {
             "name": self.name,
-            "type": "Cat",
+            "animal": "Cat",
             "nextVac": self.getNext(180),
             "nextCheck": self.getNext(180),
         }
@@ -28,7 +26,7 @@ class Rabbit(Pet):
     def getSchedule(self):
         return {
             "name": self.name,
-            "type": "Rabbit",
+            "animal": "Rabbit",
             "nextVac": self.getNext(90),
             "nextCheck": self.getNext(180),
         }
@@ -37,7 +35,7 @@ class Reptile(Pet):
     def getSchedule(self):
         return {
             "name": self.name,
-            "type": "Reptile",
+            "animal": "Reptile",
             "nextVac": self.getNext(0),
             "nextCheck": self.getNext(365),
         }
@@ -46,15 +44,39 @@ class Bird(Pet):
     def getSchedule(self):
         return {
             "name": self.name,
-            "type": "Bird",
+            "animal": "Bird",
             "nextVac": self.getNext(0),
             "nextCheck": self.getNext(730),
         }
 
-while True:
-    print("// Pet Vaccination and Health Checkup System //\n")
+def menu():
 
-    type = input("What is your pet? (dog, cat, rabbit, reptile, bird)\n> ")
+    animalArray = ['DOG','CAT','RABBIT','REPTILE','BIRD']
+    
+    print("\n[Please enter pet information]\n")
+
+    animal = input("What is your pet? (dog, cat, rabbit, reptile, bird)\n> ")
+    animal = animal.upper()
+    if animal not in animalArray:
+        print("Please enter an animal from the list presented.\n")
+        menu()
+
     name = input("Whats is your pets name?\n>")
+    
     birthDate = input("What is your pets date of birth (YYYY/MM/DD)\n> ")
+    try:
+        datetime.strptime(birthDate, "%Y/%m/%d")
+    except:
+        print("Invalid date format, Please use YYYY/MM/DD.")
+        menu()
+    
     lastVac = input("When was your pets last vaccination (YYYY/MM/DD)\n> ")
+    try:
+        validDate = datetime.strptime(birthDate, "%Y/%m/%d")
+    except:
+        print("Invalid date format, Please use YYYY/MM/DD.")
+        menu()
+
+
+print("// Pet Vaccination and Health Checkup System //")
+menu()
