a917cc3b5e85197ce140126cb135ac41914489b2
Peter Hayward <peterhayward1707@gmail.com>
2024-12-04 00:14:15 +0000

    Variables created

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+import datetime
+
+typeArray = [Dog,Cat,Rabbit,Reptile,Bird]
 
 class Pet:
     def __init__(self, name, birthDate, lastVac):
@@ -9,6 +11,8 @@ class Dog(Pet):
         return {
             "name": self.name,
             "type": "Dog",
+            "nextVac": self.getNext(365),
+            "nextCheck": self.getNext(365),
         }
 
 class Cat(Pet):
@@ -16,6 +20,8 @@ class Cat(Pet):
         return {
             "name": self.name,
             "type": "Cat",
+            "nextVac": self.getNext(180),
+            "nextCheck": self.getNext(180),
         }
 
 class Rabbit(Pet):
@@ -23,6 +29,8 @@ class Rabbit(Pet):
         return {
             "name": self.name,
             "type": "Rabbit",
+            "nextVac": self.getNext(90),
+            "nextCheck": self.getNext(180),
         }
 
 class Reptile(Pet):
@@ -30,6 +38,8 @@ class Reptile(Pet):
         return {
             "name": self.name,
             "type": "Reptile",
+            "nextVac": self.getNext(0),
+            "nextCheck": self.getNext(365),
         }
 
 class Bird(Pet):
@@ -37,5 +47,14 @@ class Bird(Pet):
         return {
             "name": self.name,
             "type": "Bird",
+            "nextVac": self.getNext(0),
+            "nextCheck": self.getNext(730),
         }
 
+while True:
+    print("// Pet Vaccination and Health Checkup System //\n")
+
+    type = input("What is your pet? (dog, cat, rabbit, reptile, bird)\n> ")
+    name = input("Whats is your pets name?\n>")
+    birthDate = input("What is your pets date of birth (YYYY/MM/DD)\n> ")
+    lastVac = input("When was your pets last vaccination (YYYY/MM/DD)\n> ")
